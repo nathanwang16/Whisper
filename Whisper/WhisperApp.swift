49ce71b093599ff63f1610b6a183672588fb5efc
Nathan Wang <nathan16@foxmail.com>
2025-05-07 21:30:24 -0700

    Initial Commit

---
+++ Whisper/WhisperApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperApp.swift
+//  Whisper
+//
+//  Created by Nathan Wang on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct WhisperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WhisperTests/WhisperTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperTests.swift
+//  WhisperTests
+//
+//  Created by Nathan Wang on 5/7/25.
+//
+
+import Testing
+@testable import Whisper
+
+struct WhisperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
